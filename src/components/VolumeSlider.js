0a0f94e16fb0e244bcdc296f4ee7b5f73d768f80
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-09-30 15:14:58 -0300

    Define initial position and style of components; Define VolumeSlider component's initial style

--- src/components/VolumeSlider.js
+++ src/components/VolumeSlider.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../style/VolumeSlider.scss'
 
 function VolumeSlider(props) {
     return (
         <div>
-            
+            <input type='range' min='0' max='100' step='1'></input>
         </div>
     )
 }
