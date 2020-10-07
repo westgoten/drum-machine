b77435af9fa974ebc7fbdcb693febfd61d45d5cb
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-10-07 20:17:42 -0300

    Set up <audio> elements on DrumPad components: Allow<audio> element to be triggered when its .drum-pad parent element is clicked. Tweak components interaction with mouse cursor

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../style/DrumPad.scss'
 
 function DrumPad(props) {
+    const audio = useRef(null)
+    function play() {
+        const audioNode = audio.current
+        audioNode.load()
+        audioNode.play()
+    }
+
     return (
-        <div id='drum-pad' className='drum-pad'>
+        <div id='drum-pad' className='drum-pad' onClick={play}>
             <span className='text'>{props.text}</span>
-            <audio id={props.text} className='clip' src=''></audio>
+            <audio id={props.text} className='clip' ref={audio} src='https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'></audio>
         </div>
     )
 }
