2a1644480c78f7155ed73f47175d6e60cde8d8e4
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-10-10 20:16:09 -0300

    Define initial app state

---
+++ src/reducers/reducers.js
@@ -0,0 +1,12 @@
+const initialState = {
+    powerOn: true,
+    currentSound: '',
+    currentVolume: 50,
+    currentInstrumentKit: ''
+}
+
+function drumMachineReducer(state = initialState, action) {
+
+}
+
+export default drumMachineReducer
