8532f4eb1c6deab48bb8fe0b667141d29fa45ce3
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-10-15 19:42:40 -0300

    Updates

---
+++ precache-manifest.59ad8aa0884b776a34e883830108937c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a62b8773a7b3b2148280f647ab38f4",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "1f752457eda1a7cb6826",
+    "url": "/drum-machine/static/css/main.d69fd7c8.chunk.css"
+  },
+  {
+    "revision": "b0b7d5b2e0616dca9e84",
+    "url": "/drum-machine/static/js/2.40b721d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/drum-machine/static/js/2.40b721d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f752457eda1a7cb6826",
+    "url": "/drum-machine/static/js/main.17b313f2.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
